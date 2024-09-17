331c19f22afe25888a5c63adcb52a789759beb49
Leta Krähenbühl <145375943+leta-krahenbuhl@users.noreply.github.com>
2024-09-17 16:51:17 +0100

    Logout button in header, start home after login

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,25 @@
 import "./Header.scss";
 import logo from "../../assets/images/logo.svg";
+import LogOut from "../LogOut/LogOut";
+
+import { jwtDecode } from "jwt-decode";
 
 export default function Header() {
+  // Retrieve the token from local storage
+  const token = localStorage.getItem("token");
+
+  // Decode the token to get the username
+  let username = "";
+  if (token) {
+    const decoded = jwtDecode(token);
+    username = decoded.username; // Access the username from the decoded token
+  }
+
   return (
     <div className="header">
       <img src={logo} alt="logo" className="landing-logo" />
-      <div className="greeting-avatar">hi</div>
+      <div className="greeting-avatar">Hi, {username ? username : "You"}!</div>
+      <LogOut />
     </div>
   );
 }
---
+++ src/components/LogOut/LogOut.jsx
@@ -0,0 +1,19 @@
+import { useNavigate } from "react-router-dom"; // Import useNavigate hook from react-router-dom
+
+export default function LogOut() {
+  const navigate = useNavigate(); // Initialize useNavigate hook
+
+  const handleLogout = () => {
+    // Remove the token from local storage
+    localStorage.removeItem("token");
+
+    // Redirect to the root path
+    navigate("/");
+  };
+
+  return (
+    <button onClick={handleLogout} className="logout-button">
+      Log Out
+    </button>
+  );
+}
