182ad7bb14ec1739aea577a50e1d073ad144a5bc
Leta Krähenbühl <145375943+leta-krahenbuhl@users.noreply.github.com>
2024-09-30 16:14:18 +0100

    Put weely slider in own component

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -6,44 +6,17 @@ import AddTasks from "../AddTasks/AddTasks";
 import WeeklyTasks from "../WeeklyTasks/WeeklyTasks";
 import OtherTasks from "../OtherTasks/OtherTasks";
 import TaskDetail from "../TaskDetail/TaskDetail";
+import WeekSlider from "../WeekSlider/WeekSlider";
 
 export default function Tasks({ homeName }) {
   const [isAddTaskOpen, setIsAddTaskOpen] = useState(false);
   const [isTaskDetailOpen, setIsTaskDetailOpen] = useState(false);
   const [selectedTask, setSelectedTask] = useState(null);
-  const [currentWeekStart, setCurrentWeekStart] = useState(
-    getMonday(new Date())
-  );
+  const [currentWeekISO, setCurrentWeekISO] = useState("");
 
-  // Current week for further use as ISO string
-  const currentWeekISO = currentWeekStart.toISOString().substring(0, 10);
-
-  // Function to get the Monday of the given date's week
-  function getMonday(date) {
-    const day = date.getDay();
-    const diff = date.getDate() - day + (day === 0 ? -6 : 1); // Adjust when day is Sunday (0)
-    return new Date(date.setDate(diff));
-  }
-
-  // Format the week start date as "WC 16th Sep 2024"
-  const formattedWeekStart = format(currentWeekStart, "'WC' do MMM yyyy");
-
-  // Handle previous week click
-  const goToPreviousWeek = () => {
-    setCurrentWeekStart((prev) => {
-      const previousWeek = new Date(prev);
-      previousWeek.setDate(previousWeek.getDate() - 7);
-      return previousWeek;
-    });
-  };
-
-  // Handle next week click
-  const goToNextWeek = () => {
-    setCurrentWeekStart((prev) => {
-      const nextWeek = new Date(prev);
-      nextWeek.setDate(nextWeek.getDate() + 7);
-      return nextWeek;
-    });
+  // set currentWeekISO
+  const setCurrentWeek = (currentWeekISO) => {
+    setCurrentWeekISO(currentWeekISO);
   };
 
   // show add task overlay
@@ -68,15 +41,7 @@ export default function Tasks({ homeName }) {
 
   return (
     <div className="tasks-all">
-      <div className="tasks-header">
-        <button onClick={goToPreviousWeek} className="tasks-button">
-          ←
-        </button>
-        <h2>{formattedWeekStart}</h2>
-        <button onClick={goToNextWeek} className="tasks-button">
-          →
-        </button>
-      </div>
+      <WeekSlider setCurrentWeek={setCurrentWeek} />
       <div className="tasks-content">
         <div className="tasks-content__headers">
           <h3 className="tasks-content__h3">Daily</h3>
---
+++ src/components/WeekSlider/WeekSlider.jsx
@@ -0,0 +1,53 @@
+import "./WeekSlider.scss";
+import { useState } from "react";
+import { format } from "date-fns";
+
+export default function WeekSlider({ setCurrentWeek }) {
+  const [currentWeekStart, setCurrentWeekStart] = useState(
+    getMonday(new Date())
+  );
+
+  // Current week for further use as ISO string
+  const currentWeekISO = currentWeekStart.toISOString().substring(0, 10);
+  setCurrentWeek(currentWeekISO);
+
+  // Function to get the Monday of the given date's week
+  function getMonday(date) {
+    const day = date.getDay();
+    const diff = date.getDate() - day + (day === 0 ? -6 : 1); // Adjust when day is Sunday (0)
+    return new Date(date.setDate(diff));
+  }
+
+  // Format the week start date as "WC 16th Sep 2024"
+  const formattedWeekStart = format(currentWeekStart, "'WC' do MMM yyyy");
+
+  // Handle previous week click
+  const goToPreviousWeek = () => {
+    setCurrentWeekStart((prev) => {
+      const previousWeek = new Date(prev);
+      previousWeek.setDate(previousWeek.getDate() - 7);
+      return previousWeek;
+    });
+  };
+
+  // Handle next week click
+  const goToNextWeek = () => {
+    setCurrentWeekStart((prev) => {
+      const nextWeek = new Date(prev);
+      nextWeek.setDate(nextWeek.getDate() + 7);
+      return nextWeek;
+    });
+  };
+
+  return (
+    <div className="week-slider">
+      <button onClick={goToPreviousWeek} className="tasks-button">
+        ←
+      </button>
+      <h2>{formattedWeekStart}</h2>
+      <button onClick={goToNextWeek} className="tasks-button">
+        →
+      </button>
+    </div>
+  );
+}
